c879ccfb1c8c50c76471fa3c02a0d1a58f4ed9c9
Lisiate <lisiate.mafi@gmail.com>
2026-06-26 16:12:23 +1300

    Added Cloudinary to Next.js image remote patterns

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,19 @@
-﻿/** @type {import('next').NextConfig} */
-const nextConfig = {
+﻿import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
       },
     ],
   },
